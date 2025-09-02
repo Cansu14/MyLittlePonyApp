52f267cadb34fc6694cb7c8d2683690a22832350
Cansu Akkaya <akkayacansu386@gmail.com>
2025-09-02 20:44:19 +0300

    refactor

--- app/src/main/java/com/example/mylittlepony/ui/screen/PonyListScreen.kt
+++ app/src/main/java/com/example/mylittlepony/ui/screen/PonyListScreen.kt
@@ -15,11 +15,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.example.mylittlepony.R
 import com.example.mylittlepony.data.PonyData
 import com.example.mylittlepony.viewmodel.PonyViewmodel
 
@@ -30,7 +32,7 @@ fun PonyListScreen(viewmodel: PonyViewmodel) {
 
     LazyColumn(
         modifier = Modifier
-            .padding(top = 36.dp, bottom = 48.dp)
+            .padding(top = 36.dp, bottom = 36.dp)
             .fillMaxWidth()
             .background(Color(0xFFfae1f9)),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -43,21 +45,26 @@ fun PonyListScreen(viewmodel: PonyViewmodel) {
 
 @Composable
 fun PonyCard(pony: PonyData) {
+    val paddingMid = dimensionResource(id = R.dimen.padding_mid)
+    val cardCorner = dimensionResource(id = R.dimen.card_corner)
+    val textSize = dimensionResource(id = R.dimen.text_size)
+    val paddingColumn = dimensionResource(id = R.dimen.padding_column)
     Card(
-        shape = RoundedCornerShape(24.dp),
+        shape = RoundedCornerShape(cardCorner),
         modifier = Modifier
-            .padding(16.dp)
+            .padding(paddingMid)
             .wrapContentWidth()
-        ) {
+    ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.background(Color(0xFFeab9fa))
-                .padding(8.dp)
+            modifier = Modifier
+                .background(Color(0xFFeab9fa))
+                .padding(paddingColumn)
         ) {
             Text(
                 pony.name,
                 color = Color(0xFF6e2885),
-                fontSize = 22.sp,
+                fontSize = textSize.value.sp,
                 fontFamily = FontFamily.Cursive,
                 textAlign = TextAlign.Center,
             )
@@ -66,7 +73,7 @@ fun PonyCard(pony: PonyData) {
                 model = pony.image[0],
                 contentDescription = pony.name,
                 modifier = Modifier
-                    .clip(RoundedCornerShape(24.dp))
+                    .clip(RoundedCornerShape(cardCorner))
             )
         }
     }
